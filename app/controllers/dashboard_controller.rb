5de52b7fee3e46401d7aa58448c0bc650e48c046
dalfaro7 <dalfaro7@gmail.com>
2026-06-06 00:02:23 -0600

    nuevo balance de roll

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,34 +12,42 @@ class DashboardController < ApplicationController
     end
 
     month_range = @month.beginning_of_month..@month.end_of_month
+    assigned_statuses = [:worked, :assigned_task]
 
     @guides = Guide.active.order(:name)
 
-    worked_days_by_guide_id = GuideDay
+    assigned_days_by_guide_id = GuideDay
       .joins(:work_day)
-      .where(guide: @guides, status: :worked)
+      .where(guide: @guides, status: assigned_statuses)
       .where(work_days: { date: month_range })
       .group(:guide_id)
       .count
 
-    @total_worked = @guides.sum do |guide|
-      worked_days_by_guide_id[guide.id].to_i
+    @assigned_work_slots = @guides.sum do |guide|
+      assigned_days_by_guide_id[guide.id].to_i
     end
+
+    @worked_month_days = GuideDay
+      .joins(:work_day)
+      .where(status: assigned_statuses)
+      .where(work_days: { date: month_range })
+      .distinct
+      .count("work_days.date")
 
     @average =
       if @guides.any?
-        @total_worked.to_f / @guides.size
+        (@assigned_work_slots.to_f / @guides.size).round
       else
         0
       end
 
     @dashboard_data = @guides.map do |guide|
-      worked = worked_days_by_guide_id[guide.id].to_i
+      worked = assigned_days_by_guide_id[guide.id].to_i
 
       percentage =
-        @total_worked > 0 ? (worked.to_f / @total_worked * 100).round(2) : 0
+        @assigned_work_slots > 0 ? (worked.to_f / @assigned_work_slots * 100).round(2) : 0
 
-      deviation = (worked - @average).round(2)
+      deviation = worked - @average
 
       {
         guide: guide,
@@ -61,21 +69,21 @@ class DashboardController < ApplicationController
       end
 
     @historical_data =
-  if @selected_guide.present?
-    GuideDay
-      .joins(:work_day)
-      .where(guide: @selected_guide, status: :worked)
-      .group(Arel.sql("DATE_TRUNC('month', work_days.date)"))
-      .order(Arel.sql("DATE_TRUNC('month', work_days.date)"))
-      .count
-      .map do |month_date, worked_days|
-        [
-          month_date.strftime("%Y-%m"),
-          worked_days.to_i
-        ]
+      if @selected_guide.present?
+        GuideDay
+          .joins(:work_day)
+          .where(guide: @selected_guide, status: assigned_statuses)
+          .group(Arel.sql("DATE_TRUNC('month', work_days.date)"))
+          .order(Arel.sql("DATE_TRUNC('month', work_days.date)"))
+          .count
+          .map do |month_date, assigned_days|
+            [
+              month_date.strftime("%Y-%m"),
+              assigned_days.to_i
+            ]
+          end
+      else
+        []
       end
-  else
-    []
-  end
   end
 end
