366d4a9c3955bad78e111bebd35415ecf4bbb448
dalfaro7 <dalfaro7@gmail.com>
2026-08-03 18:00:00 -0600

    acumulados y vacaciones

--- db/migrate/20260803231047_add_office_day_credit_to_office_employee_days.rb
+++ db/migrate/20260803231047_add_office_day_credit_to_office_employee_days.rb
@@ -1,5 +1,5 @@
 class AddOfficeDayCreditToOfficeEmployeeDays < ActiveRecord::Migration[8.1]
   def change
-    add_reference :office_employee_days, :office_day_credit, null: false, foreign_key: true
+    add_reference :office_employee_days, :office_day_credit, null: true, foreign_key: true
   end
 end
